b5288b8fd0301f9d506d5b33bd180c311f1098cd
adm_chermeninna <adm_chermeninna@NAKNGOK-U000118>
2017-05-18 13:07:42 +0900

    Добавьте файлы проекта.

---
+++ AirFlight/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AirFlight.Startup))]
+namespace AirFlight
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
